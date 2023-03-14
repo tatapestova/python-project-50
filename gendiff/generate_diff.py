036b5a93383da37e1281bc5c155a8ba2637e75e0
root <tata.pestova@inbox.ru>
2023-03-14 22:07:26 +0300

    after 2 check

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -9,21 +9,21 @@ def genetare_sorted_diff(file1, file2):
                 'key': key,
                 'status': 'DELETED',
                 'value': file1[key]
-                }
+            }
             diff.append(node)
         elif key in file2 and key not in file1:
             node = {
                 'key': key,
                 'status': 'ADDED',
                 'value': file2[key]
-                }
+            }
             diff.append(node)
         elif file1[key] == file2[key]:
             node = {
                 'key': key,
                 'status': 'SAME',
                 'value': file1[key]
-                }
+            }
             diff.append(node)
         elif isinstance(file1[key], dict) and isinstance(file2[key], dict):
             diff_clild = genetare_sorted_diff(file1[key], file2[key])
@@ -31,7 +31,7 @@ def genetare_sorted_diff(file1, file2):
                 'key': key,
                 'status': 'NESTED',
                 'value': diff_clild
-                }
+            }
             diff.append(node)
         else:
             node = {
@@ -39,6 +39,6 @@ def genetare_sorted_diff(file1, file2):
                 'status': 'DIFF',
                 'value1': file1[key],
                 'value2': file2[key]
-                }
+            }
             diff.append(node)
     return diff
